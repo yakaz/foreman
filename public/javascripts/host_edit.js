606940daaa2510714d7e8dabe9d6ff5d4d4b602e
Olivier Favre <olivier@yakaz.com>
2012-07-16 15:54:24 +0200

    Fix modal dialog size

--- public/javascripts/host_edit.js
+++ public/javascripts/host_edit.js
@@ -223,12 +223,17 @@ function smart_var_dialog(item) {
       '<div class="controls"><img src="/images/edit.png" alt="Currently under edition..." /></div>'+
       '</div>');
   target.replaceWith(placeholder);
-  var box = $('<div class="modal-header modal fade"><a class="close" data-dismiss="modal">&times;</a><h3>'+$(item).prevAll('span').text()+'</h3></div>');
-  box.on('hide', function() {
+  target.children('.control-label').addClass('hide');
+  var box = $('<div class="modal scrollable modal10 fade"><a class="close" data-dismiss="modal">&times;</a><h3 class="modal-header">'+$(item).prevAll('span').text()+'</h3></div>');
+  box.on('hidden', function() {
+    target.children('.control-label').removeClass('hide');
+    target.removeClass('modal-body');
     placeholder.replaceWith(target);
     placeholder.remove();
+    box.remove();
   });
   box.append(target.detach());
+  target.addClass('modal-body');
   box.modal('show');
 }
 
